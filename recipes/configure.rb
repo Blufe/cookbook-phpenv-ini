2cf4c0d3f7950c22d259307f6c5c0a6118af4ca0
Blufe <bluephoenixlab@gmail.com>
2015-10-14 18:58:37 +0900

    create error_log folder

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -19,5 +19,14 @@ Array(node[:phpenv][:phps]).each do |php|
       variables(conf)
       notifies :reload, "service[apache2]", :delayed
     end
+
+    directory File.dirname(conf["error_log"]) do
+      owner     "root"
+      group     "root"
+      recursive true
+      mode      0777
+      action    :create
+      not_if { conf["error_log"].nil? or File.exists?(File.dirname(conf["error_log"].to_s)) }
+    end
   end
 end
